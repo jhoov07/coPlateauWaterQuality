a10312386c781fcdf67e5663b6611691fe2bd089
austinmartinez12 <amartinez890@toromail.csudh.edu>
2024-06-25 13:13:03 -0700

    Update AustinboostedRegressionTree.R

--- 02_scripts/AustinboostedRegressionTree.R
+++ 02_scripts/AustinboostedRegressionTree.R
@@ -1,8 +1,8 @@
-setwd("~/Desktop/Biosphere 2/Arsenic data")
 Asdata = read.csv("AsModelInput.csv")
 
 #setwd("C:/Users/jhoover/Documents/GitHub/coPlateauWaterQuality/01_data")
-#setwd("C:/Users/austinmartinez/Documents/GitHub/coPlateauWaterQuality/01_data")
+setwd("C:/Users/austinmartinez/Documents/GitHub/coPlateauWaterQuality/01_data")
+Asdata = read.csv("AsModelInput.csv")
 
 # Filter data into train and test sets based on logical variable 'spl3cat'
 train <- Asdata[Asdata$spl5 == TRUE, ]
@@ -16,7 +16,6 @@ test <- Asdata[Asdata$spl5 == FALSE, ]
 # Drop rows with NA values in columns 9 to 93 for test data
 AsTrain <- train[complete.cases(train[, 9:93]), ]
 AsTest <- test[complete.cases(test[, 9:93]), ]
-
 Clean_As <- rbind(AsTrain ,AsTest)
 
 #Ensure As3Cat is a Factor (Categorical Variable)
@@ -25,19 +24,21 @@ AsTest$As3Cat <- as.factor(AsTest$As3Cat)
 Clean_As$As3Cat <- as.factor(Clean_As$As3Cat)
 
 
-Arsenic_boost = train(
-  factor(Clean_As[,4])~.,
-  data = Clean_As[,-(1:8)],
-  method = "gbm",
+Arsenic_boost <- train(
+  As3Cat ~ .,  
+  data = Clean_As[, -c(1:3, 5:8)],
+  method = "gbm", 
   trControl = trainControl(
     method = "cv",
-    number = 5
+    number = 5,
+    verboseIter = TRUE  # Enable verbose output for troubleshooting
   ),
   tuneGrid = expand.grid(
-    "n.trees" = seq(25, 200, by = 25),
-    "interaction.depth" = 1:3,
-    "shrinkage" = c(0.1, 0.01, 0.001),
-    "n.minobsinnode" = 5)
+    n.trees = seq(50, 200, by = 50),
+    interaction.depth = 1:3,
+    shrinkage = c(0.1, 0.01, 0.001),
+    n.minobsinnode = 5
+  )
 )
 
 
