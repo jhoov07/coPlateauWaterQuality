a064dc18109804a457593aff9e6156183c35d036
jhoov07 <156460040+jhoov07@users.noreply.github.com>
2024-07-02 23:16:58 -0700

    Update AustinXGboost.R
    Updated code to create DMatrix and run xbg.train using binary logistic regreission

--- 02_scripts/AustinXGboost.R
+++ 02_scripts/AustinXGboost.R
@@ -4,14 +4,52 @@ library(gbm)
 library(xgboost) # for xgboost
 library(tidyverse) # general utility functions
 
-#setwd("C:/Users/jhoover/Documents/GitHub/coPlateauWaterQuality/01_data")
+setwd("/Users/hoover/Documents/GitHub/coPlateauWaterQuality/01_data")
 #setwd("C:/Users/austinmartinez/Documents/GitHub/coPlateauWaterQuality/01_data")
 
+#Clean up the workspace
+rm(list=ls())
+
+#Load data
 All_Asdata = read.csv("AsModelInput.csv")
+
 # set a random seed & shuffle data frame
 set.seed(1234)
 Asdata <- All_Asdata[sample(1:nrow(All_Asdata)), ]
+#Asdata<-as.data.frame(Asdata)
 
+# get the numb 70/30 training test split
+#split into training (70%) and testing set (30%)
+sample_set<-sample(nrow(Asdata), round(nrow(Asdata)*.7), replace = F)
+As_train = Asdata[sample_set,]
+As_test = Asdata[-sample_set,]
+
+#Complete cases
+As_trainComp <- As_train[complete.cases(As_train[,c(3,9:93)]),]  #col 3, testing on As >10 ug/L category
+As_testComp<- As_test[complete.cases(As_test[,c(3,9:93)]),] #col 3testing on As >10 ug/L category
+
+#define predictor and response variables in training set
+train_x<-data.matrix(As_trainComp[, -c(1:8)])
+train_y<-As_trainComp[,3]
+
+#define predictor and response variables in testing set
+test_x<-data.matrix(As_testComp[, -c(1:8)])
+test_y<-As_testComp[,3]
+
+#define final training and testing sets
+xgb_train = xgb.DMatrix(data = train_x, label = train_y)  #OK, this runs when the outcome variable is numeric
+xgb_test = xgb.DMatrix(data = test_x, label = test_y)
+
+#define watchlist
+watchlist = list(train=xgb_train, test=xgb_test)
+
+#fit XGBoost model and display training and testing data at each round
+model = xgb.train(data = xgb_train, max.depth = 4, watchlist=watchlist, nrounds = 70, objective = "binary:logistic")
+
+
+
+
+##I didn't use this code below
 #turning all 1-0(true and false) to booleans
 # List of non-numeric variables
 non_numeric_vars <- c(
@@ -40,12 +78,16 @@ FullcleanAs <- cbind(AsLabels, clean_Asdata)
 # Ensure the result is a data frame
 FullcleanAs <- as.data.frame(FullcleanAs)
 
-# make it a metrix
-As_matrix <- FullcleanAs %>%
-  data.matrix()
-
 # get the numb 70/30 training test split
-numberOfTrainingSamples <- round(length(Asdata) * .7)
+#split into training (70%) and testing set (30%)
+sample_set<-sample(nrow(FullcleanAs), round(nrow(FullcleanAs)*.7), replace = F)
+As_train = FullcleanAs[sample_set,]
+As_test = FullcleanAs[-sample_set,]
+
+# make it a metrix
+#As_matrix <- FullcleanAs %>%
+#  data.matrix()
+
 
 # As3Cat is the target variable
 train_labels <- All_Asdata$As3Cat 
