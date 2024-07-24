ff73ec21edef27c2c8fc095cb41d2f2ef7f16e2a
austinmartinez12 <amartinez890@toromail.csudh.edu>
2024-07-24 10:56:06 -0700

    Update As_GIS_datacleaning.R
    Added code to make C1,C2,C3 for As3Cat in BRT

--- 02_scripts/dataCleaning/As_GIS_datacleaning.R
+++ 02_scripts/dataCleaning/As_GIS_datacleaning.R
@@ -48,7 +48,47 @@ data <- data %>%
     bas10 = ifelse(ResultMeasureValue > 10, 1, 0)
   )
 
-# - "US_L3NAME", -"ORIG_LABEL", "FID_Surficial_materials", "US_L3CODE", "UNIT_CODE ect."
+data <- data %>%
+  mutate(
+    As3Cat = case_when(
+      ResultMeasureValue > 10 ~ "C3",
+      ResultMeasureValue > 5 ~ "C2",
+      ResultMeasureValue >= 1 ~ "C1",
+      TRUE ~ "C1"  # Values below 1 will also be C1
+    )
+  )
+
+set.seed(1234)
+
+data <- data[sample(1:nrow(data)), ]
+
+# get the numb 70/30 training test split
+#split into training (70%) and testing set (30%)
+sample_set<-sample(nrow(data), round(nrow(data)*.7), replace = F)
+data_train = data[sample_set,]
+data_test = data[-sample_set,]
+
+# Add the spl3cat column
+data$spl3cat <- FALSE
+data$spl3cat[sample_set] <- TRUE
+table(data$spl3cat)
+
+set.seed(1234)
+
+data <- data[sample(1:nrow(data)), ]
+
+# get the numb 70/30 training test split
+#split into training (70%) and testing set (30%)
+sample_set<-sample(nrow(data), round(nrow(data)*.7), replace = F)
+data_train = data[sample_set,]
+data_test = data[-sample_set,]
+
+data$spl5 <- FALSE
+data$spl5[sample_set] <- TRUE
+table(data$spl5)
+
+
+# - "US_L3NAME", -"ORIG_LABEL", "FID_Surficial_materials", "US_L3CODE", "UNIT_CODE"
 data <- subset(data, select = -c(WellHoleDepthMeasureValue, Built, WellDepthMeasureUnitCode, US_L3NAME, WellHoleDepthMeasureUnitCode, UNIT_NAME, ResultMeasureMeasureUnitCode, ORIG_LABEL, ElevationUnitCode, FID_Surficial_materials, US_L3CODE, UNIT_CODE, GENERALIZE))
 
 write.csv(data, file = "~/Desktop/Cleaned_As_GIS_Filtered.csv", row.names = FALSE)
