9f17f5b4802c7c1f2c45c6a6bf1fdfd855ff1610
jhoov07 <156460040+jhoov07@users.noreply.github.com>
2024-07-24 20:25:52 -0700

    Updates

--- 02_scripts/brt/step2_brt_as_modelProcessing_forHPC.R
+++ 02_scripts/brt/step2_brt_as_modelProcessing_forHPC.R
@@ -4,7 +4,7 @@ library(caret)
 library(tidyverse)
 
 #Set working directory
-setwd("/Users/hoover/Documents/GitHub/coPlateauWaterQuality/01_data")
+setwd("/Users/hoover/Documents/GitHub/coPlateauWaterQuality/01_data/CoPlateau_As")
 #setwd("/Users/austinmartinez/Documents/GitHub/coPlateauWaterQuality/01_data")
 
 #Clean up the workspace
@@ -14,7 +14,7 @@ date<-Sys.Date()
 set.seed(1234)  # Setting seed 
 
 #Load data
-Asdata = read.csv("./CoPlateau_As/20240723_randomForest_As_dataClean.csv",
+Asdata = read.csv("./2_20240724_randomForest_As_dataClean.csv",
                   na.strings = "NA") #Probably need to simplify the path so the script and data are in the same folder for the HPC
 
 Asdata <- Asdata[complete.cases(Asdata), ]
@@ -37,7 +37,7 @@ Arsenic_boost <- train(
   method = "gbm", 
   trControl = trainControl(
     method = "cv",
-    number = 10,    #Use 10 fold for real runs
+    number = 1,    #Use 10 fold for real runs
     verboseIter = TRUE  # Enable verbose output for troubleshooting
   ),
   tuneGrid = expand.grid(
