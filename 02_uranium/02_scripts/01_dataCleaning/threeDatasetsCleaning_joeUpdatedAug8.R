3c213970f190de3526d1ca88c8c33014f3bfd540
aaronnuanez <aaronnuanez@arizona.edu>
2024-08-16 12:13:28 -0700

    Merge branch 'main' of https://github.com/jhoov07/coPlateauWaterQuality

--- 02_uranium/02_scripts/01_dataCleaning/threeDatasetsCleaning_joeUpdatedAug8.R
+++ 02_uranium/02_scripts/01_dataCleaning/threeDatasetsCleaning_joeUpdatedAug8.R
@@ -1,5 +1,5 @@
 #setwd("~/Desktop")
-setwd("/Users/aaronnuanez/Documents/GitHub/coPlateauWaterQuality")
+setwd("/Users/aaronnuanez/HooverShare/Shared Group Data/20_projects/05_coPlateau")
 
 #Load libraries
 library(tidyverse)
@@ -9,41 +9,43 @@ library(dplyr)
 rm(list=ls())
 
 #Load data
-data <- read.csv("./02_uranium/01_data/Nure7_Data_ExportTable.csv")
-data2 <- read.csv("./02_uranium/01_data/wqpData_cleaned_20240808.csv", na.strings = "NULL")
-data3 <- read.csv("./02_uranium/01_data/nnwells3_ExportTable.csv")
+Nure <- read.csv("./02_uranium/01_data/Nure7_Data_ExportTable.csv")
+WQP <- read.csv("./02_uranium/01_data/wqpData_cleaned_20240808.csv", na.strings = "NULL")
+NNwells <- read.csv("./02_uranium/01_data/nnwells3_Check_ExportTable.csv")
 
 #Rename fields in NURE dataset
-data<- data %>% 
+Nure <- Nure %>% 
   rename(baseflow = bfi48grd_ProjectRaster2, prism30yr = PRISM_ppt_30yr_ProjectRaster1, U = u_fl_ppb)
 
-#Rename fields in NN Wells Data
-data2<- data2 %>% 
+#Rename fields in WQP Data
+WQP<- WQP %>% 
   rename(baseflow = bfi48grd,
          prism30yr = PRISM_30yrNorm,
          welldpth = WellDepthMeasureValue,
-         As = ResultMeasureValue)
+         As = ResultMeasureValue,
+         F30mElevationFoCo = F30mElevat)
 
-#Rename fields in data from Saurav
-data3<- data3 %>% 
+#Rename fields in NNwells data
+NNwells<- NNwells %>% 
   rename(welldpth = depth,
          As = As_,
-         Fl = Fl_combined)
-
+         Fl = Fl_combine)
+#table(data3$Fl)
 ##Concatenate data source name to SiteID/WellID/Record Number
-data$SiteID<-paste("nure-",data$rec_no, sep="")
-data2$SiteID<-paste("nnWells-",data2$SiteID, sep="")
-data3$SiteID<-paste("wtrQalPort-",data3$well_id, sep="")
+Nure$SiteID<-paste("nure-",Nure$rec_no, sep="")
+WQP$SiteID<-paste("nnWells-",WQP$SiteID, sep="")
+NNwells$SiteID<-paste("wtrQalPort-",NNwells$well_id, sep="")
 
 #Update field types for a few variables so they match
-#nnwells$welldpth <- as.numeric(nnwells$welldpth)
-#Nure6$welldpth <- as.numeric(Nure6$welldpth)
-#Asdata$welldpth <- as.numeric(Asdata$welldpth)
+#data$SiteID <- as.numeric(data$SiteID)
+#data2$SiteID <- as.numeric(data2$SiteID)
+#data3$SiteID <- as.numeric(data3$SiteID)
 
-
-
+#str(NNwells$SiteID)
 #combine data sets
-combined_data <- bind_rows(data, data2, data3)
+combined_data <- bind_rows(Nure, WQP)
+
+combined_data1 <- bind_rows(combined_data, NNwells)#
 
 # removes specified columns
 combined_data <- subset(combined_data, select = -c(OID_, rec_no, CharacteristicName, ResultSampleFractionText, well_id, code, bas1, bas5, bas10, As3Cat, spl3cat, spl5))
@@ -55,19 +57,20 @@ combinedData_reorder<-combined_data[,c(218, 2, 219, 222, 220, 1, 3:217, 221, 223
 # Replace NAs with 0s
 
 
-write.csv(combined_data, file = "~/Desktop/2combined_welldata.csv", row.names = FALSE)
+write.csv(combined_data, file = "~/Desktop/3combined_welldata.csv", row.names = FALSE)
 
 
 #Need to find missing fields then create new blank fields, i started by comparing data and data1, need to repeat for data2
-#a<-colnames(data)
-#b<-colnames(data2)
-#c<-colnames(data3)
+a<-colnames(Nure)
+b<-colnames(WQP)
+c<-colnames(NNwells)
 
-##setdiff(a, b) #this will tell you the fields that don't match between a and b, anything not matching needs to be added to b
+#setdiff(a, b) #this will tell you the fields that don't match between a and b, anything not matching needs to be added to b, shows what data is in a that is not in b
 #setdiff(b, a) #this will tell you the fields that don't match between b and a, anything not matching needs to be added to a
 #setdiff(a, c)
 #setdiff(b, c)
 #setdiff(c, a)
+#setdiff(c, b)
 
 
 #Add in missing columns so all datasets have the same columns. 
