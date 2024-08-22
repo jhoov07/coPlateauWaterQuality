1cf30a95e589f7e90347938da527ab42d07dff45
aaronnuanez <aaronnuanez@arizona.edu>
2024-08-22 11:46:08 -0700

    nnwells update and create WQP analytes code

---
+++ 01_arsenic/02_scripts/WQP_analytes.R
@@ -0,0 +1,19 @@
+setwd(Set to Nas"05_coPlateauWaterQuality/01_arsenic"")
+
+#Load libraries
+#library(tidyverse)
+library(dplyr)
+
+Fe <- read.csv("/01_data_.iron.csv")
+Ca <- read.csv("./01_data/Ca.csv")
+Alkalinity <- read.csv("./01_data/Alkalinity.csv")
+pH <- read.csv("./01_data/pH.csv")
+
+data2 <- data %>%
+  filter(analyte == "As" |
+  
+  
+  
+  
+  
+write.csv(combined_data, file = "~/Desktop/#combined_analytes_nnwells.csv", row.names = FALSE)
