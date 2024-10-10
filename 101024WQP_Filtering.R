716fd521dfc58dc5881e88a11c718e6dbabd204a
aaronnuanez <aaronnuanez@arizona.edu>
2024-10-10 16:57:45 -0700

    updates

--- 101024WQP_Filtering.R
+++ 101024WQP_Filtering.R
@@ -20,9 +20,15 @@ f2<- f %>%
   filter(ResultSampleFractionText == "Total") %>%
   mutate()
 
-#for alkalinity and pH: check activity media name is water, checkresult sample fraction text to be "total", check result measure measure unit code is "std units", if anything else drop it 
+#for alkalinity and pH: check activity media name is water, check result sample fraction text to be "total", check result measure measure unit code is "std units", if anything else drop it 
+#Alkalinity: filter to only "total" in ResultSampleFractionText, convert ug/L to mg/L in ResultMeasureMeasureUnitCode
+#pH:filter to only "total" in ResultSampleFractionText, remove "NULL" in ResultMeasureMeasureUnitCode
 
-#for iron, calcium, Uranium: check activity media name is water, checkresult sample fraction text to be "dissolved", check result measure measure unit code is "ug/L" (see if units make sense then do unit conversions)
+#for iron, calcium, Uranium: check activity media name is water, check result sample fraction text to be "dissolved", check result measure measure unit code is "ug/L" (see if units make sense then do unit conversions)
+#Iron: filter to only dissolved in ResultSampleFractionText,
+#Calcium: filter to only dissolved in ResultSampleFractionText, keep mg/L
+#Uranium: AZ (keep "U" and "Uranium-238" in CharacteristicName), NM (keep "U" and "Uranium-238" in CharacteristicName)
+
 
 #Merge files
 cdef<-rbind(c,d,e,f)
@@ -32,3 +38,5 @@ length(unique(cdef$SiteID))
 #convert to wide format
 wide<-dcast(cdef, SiteID~CharacteristicName+ResultMeasureMeasureUnitCode, value.var="ResultMeasureValue", max)
 #write to csv
+
+write.csv("", file = "~/Desktop/.csv", row.names = FALSE)
--- NNwells_analytes_combine.R
+++ NNwells_analytes_combine.R
@@ -5,17 +5,32 @@ dataanalytes <- read.csv("./02_Data/Raw_Data/20241003_nnwellsAnalytes.csv")
 
 merged_nnwells_tables <- merge(datawells, dataanalytes, by = "well_id", all.x=TRUE)
 
-#Omit rows where no NNwell data is present
-#omit_merged_nnwells_tables <- subset(merged_nnwells_tables, OID_ != "NA")
-
-#Need to omit rows where no Analytes data is present 
-
 ##Reorder
 abc<-merged_nnwells_tables[,c(1,220:230,2:218)]
 
 ##Drop unused field
 abc2<-abc[,-c(13:23, 26:30, 32:38, 40:56, 58:59, 229)]
 
+#GENERALIZE, bedrock geology
+unique_vals <- unique(data$GENERALIZE)
+for (val in unique_vals) {
+  col_name <- paste("GENERALIZE", gsub(" ", "_", gsub(",", "", val)), sep = "_")
+  data[[col_name]] <- ifelse(data$GENERALIZE == val, 1, 0)
+}
 
-write.csv(merged_nnwells_tables, file = "~/Desktop/100924MergedNNwells.csv", row.names = FALSE)
+#UNIT_NAME
+unique_vals <- unique(data$UNIT_NAME)
+for (val in unique_vals) {
+  col_name <- paste("UNIT_NAME", gsub(" ", "_", gsub(",", "", val)), sep = "_")
+  data[[col_name]] <- ifelse(data$UNIT_NAME == val, 1, 0)
+}
+
+#US_L3NAME
+unique_vals <- unique(data$US_L3NAME)
+for (val in unique_vals) {
+  col_name <- paste("US_L3NAME", gsub(" ", "_", gsub(",", "", val)), sep = "_")
+  data[[col_name]] <- ifelse(data$US_L3NAME == val, 1, 0)
+}
+
+write.csv(abc2, file = "~/Desktop/101024MergedNNwells.csv", row.names = FALSE)
 
