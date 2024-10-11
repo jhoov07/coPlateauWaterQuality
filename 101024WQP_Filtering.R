adbdf0b4874b6f180d598d3f221e86c82890d688
aaronnuanez <aaronnuanez@arizona.edu>
2024-10-11 12:45:30 -0700

    update

--- 101024WQP_Filtering.R
+++ 101024WQP_Filtering.R
@@ -16,18 +16,51 @@ e<-read.csv("./02_Data/Raw_Data/WQP/00_archive/ph.csv", na.strings = "NULL")
 f<-read.csv("./02_Data/Raw_Data/WQP/00_archive/alkalinity.csv", na.strings = "NULL")
 
 #Process the files
-f2<- f %>%
+
+#Alkalinity: convert ug/L to mg/L in ResultMeasureMeasureUnitCode (need to do)
+f2 <- f %>%
   filter(ResultSampleFractionText == "Total") %>%
-  mutate()
+  mutate() 
+f3 <- f2 %>% filter(ResultMeasureMeasureUnitCode != "NULL")
+#ug_to_mg <-0.001
+#f4 <- f3 %>%
+#  mutate(
+#    ResultMeasureValue = ifelse(ResultMeasureMeasureUnitCode == "ug/L", ResultMeasureValue * ug_to_mg, ResultMeasureValue)  # Convert values
+#    ResultMeasureMeasureUnitCode = ifelse(ResultMeasureMeasureUnitCode == "ug/L", "mg/L", ResultMeasureMeasureUnitCode)
+#write.csv(f4, file = "~/Desktop/alkalinitycheck.csv", row.names = FALSE)
 
-#for alkalinity and pH: check activity media name is water, check result sample fraction text to be "total", check result measure measure unit code is "std units", if anything else drop it 
-#Alkalinity: filter to only "total" in ResultSampleFractionText, convert ug/L to mg/L in ResultMeasureMeasureUnitCode
-#pH:filter to only "total" in ResultSampleFractionText, remove "NULL" in ResultMeasureMeasureUnitCode
+#pH 
+e2 <- e %>%
+  filter(ResultSampleFractionText == "Total") %>%
+  mutate() 
+e3 <- e2 %>% filter(ResultMeasureMeasureUnitCode != "NULL")
+#write.csv(e3, file = "~/Desktop/pHcheck.csv", row.names = FALSE)
 
-#for iron, calcium, Uranium: check activity media name is water, check result sample fraction text to be "dissolved", check result measure measure unit code is "ug/L" (see if units make sense then do unit conversions)
-#Iron: filter to only dissolved in ResultSampleFractionText,
-#Calcium: filter to only dissolved in ResultSampleFractionText, keep mg/L
+#Iron: unit conversions (need to do)
+d2 <- d %>%
+  filter(ResultSampleFractionText == "Total") %>%
+  mutate() 
+#write.csv(d2, file = "~/Desktop/ironcheck.csv", row.names = FALSE)
+
+#Calcium: keep mg/L (need to do)
+c2 <- c %>%
+  filter(ResultSampleFractionText == "Dissolved") %>%
+  mutate() 
+write.csv(c2, file = "~/Desktop/calciumcheck.csv", row.names = FALSE)
+
 #Uranium: AZ (keep "U" and "Uranium-238" in CharacteristicName), NM (keep "U" and "Uranium-238" in CharacteristicName)
+a2 <- a %>% 
+  filter(CharacteristicName == "Uranium-238" |
+        CharacteristicName == "U") %>%
+  mutate() 
+a3 <- a2 %>% filter(ResultMeasureValue != "NULL")
+b2 <- b %>%
+  filter(CharacteristicName == "Uranium-238" |
+        CharacteristicName == "U") %>%
+  mutate()
+b3 <- b2 %>% filter(ResultMeasureValue != "NULL")
+write.csv(a3, file = "~/Desktop/AZ_Ucheck.csv", row.names = FALSE)
+write.csv(b3, file = "~/Desktop/NM_Ucheck.csv", row.names = FALSE)
 
 
 #Merge files
