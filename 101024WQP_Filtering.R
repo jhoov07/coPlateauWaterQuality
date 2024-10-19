8c24e91201411239340f9a898d7994609aafff76
jhoov07 <156460040+jhoov07@users.noreply.github.com>
2024-10-18 17:42:19 -0700

    Updated WQP cleaning script

--- 101024WQP_Filtering.R
+++ 101024WQP_Filtering.R
@@ -12,8 +12,8 @@ setwd("/Volumes/HooverShare/Shared_Group_Data/20_projects/06_coPlateau_Rework/")
 rm(list=ls())
 
 #Load csv files
-a<-read.csv("./02_Data/Raw_Data/WQP/00_archive/AZ_Uranium.csv")
-b<-read.csv("./02_Data/Raw_Data/WQP/00_archive/NM_Uranium.csv")
+a<-read.csv("./02_Data/Raw_Data/WQP/00_archive/AZ_Uranium.csv", na.strings = "NULL")
+b<-read.csv("./02_Data/Raw_Data/WQP/00_archive/NM_Uranium.csv", na.strings = "NULL")
 c<-read.csv("./02_Data/Raw_Data/WQP/00_archive/Ca.csv", na.strings = "NULL")
 d<-read.csv("./02_Data/Raw_Data/WQP/00_archive/Iron.csv", na.strings = "NULL")
 e<-read.csv("./02_Data/Raw_Data/WQP/00_archive/ph.csv", na.strings = "NULL")
@@ -22,25 +22,21 @@ g<-read.csv("./02_Data/Raw_Data/WQP/00_archive/CO_Uranium.csv", na.strings = "NU
 h<-read.csv("./02_Data/Raw_Data/WQP/00_archive/UT_Uranium.csv", na.strings = "NULL")
 
 #Process the files
-#Uranium: AZ (keep "U" and "Uranium-238" in CharacteristicName), NM (keep "U" and "Uranium-238" in CharacteristicName)
-a2 <- a %>% 
-  select(a, c("SampleID", "CharacteristicName", "ResultSampleFractionText", "ResultMeasureValue", "ResultMeasureMeasureUnitCode") %>%
-  #a[, c("SampleID", "CharacteristicName", "ResultSampleFractionText", "ResultMeasureValue", "ResultMeasureMeasureUnitCode")] %>%
+#Merge U files into a single data frame (done)
+u<-rbind(a,b,g,h)
+
+#Process uranium data (done)
+u2 <- u %>%
   drop_na(ResultMeasureMeasureUnitCode) %>%
-  filter(CharacteristicName == "Uranium-238" | CharacteristicName == "U")
+  filter(ResultSampleFractionText == "Dissolved") %>%
+  filter(ResultMeasureMeasureUnitCode != "ratio") %>%
+  filter(CharacteristicName == "U")
   
-  write.csv(a2, file = "~/Desktop/AZ_U.csv", row.names = FALSE)  
   
-#b2 <- b %>%
-#  filter(CharacteristicName == "Uranium-238" |
-#           CharacteristicName == "U") %>%
-#  mutate()
-#b3 <- b2 %>% filter(ResultMeasureValue != "NULL")
-
-
-
-
-
+summary(factor(u2$ResultMeasureMeasureUnitCode)) #Check to see what units are noted in the field
+mgL_indices <- which(u2$ResultMeasureMeasureUnitCode == "pCi/L") #Create an index with records that we need to convert
+u2$ResultMeasureValue[mgL_indices] <- u2$ResultMeasureValue[mgL_indices] * 0.67 #Convert to pCi/L to ug/L match NN Wells analyte data
+u2$ResultMeasureMeasureUnitCode <- "ug/L"   # made all units mg/L
 
 
 #Calcium: keep mg/L (done)
@@ -74,7 +70,9 @@ e2 <- e %>%
 #Alkalinity: convert ug/L to mg/L in ResultMeasureMeasureUnitCode (done)
 f2 <- f %>%
   drop_na(ResultMeasureMeasureUnitCode) %>%   #Remove rows with NA's using drop_na()
-  filter(ResultSampleFractionText == "Total") #filter to total results since we want to use raw water samples
+  filter(ResultSampleFractionText == "Total") %>% #filter to total results since we want to use raw water samples
+  filter(CharacteristicName == "Alkalinity")
+
 
 summary(factor(f2$ResultMeasureMeasureUnitCode)) #Check to see what units are noted in the field
 mgL_indices <- which(f2$ResultMeasureMeasureUnitCode == "ug/l") #Create an index with records that we need to convert
@@ -82,9 +80,9 @@ f2$ResultMeasureValue[mgL_indices] <- f2$ResultMeasureValue[mgL_indices] / 1000 
 f2$ResultMeasureMeasureUnitCode <- "mg/L"   # made all units mg/L
 
 #Merge files
-cdef<-rbind(c2,d2,e2,f2)
+cdef<-rbind(c2,d2,e2,f2, u2)
 
-cdef <- cdef %>%
+cdef2 <- cdef %>%
   filter(StateCode == 4 & (CountyCode == 1 | CountyCode == 5 | CountyCode == 7 | CountyCode == 15 | CountyCode == 17 | CountyCode == 25) |
            (StateCode == 35 & (CountyCode == 3 | CountyCode ==6 | CountyCode ==31 | CountyCode ==39 | CountyCode ==43 | CountyCode ==45)) |
            (StateCode == 08 & (CountyCode == 29 | CountyCode == 33 | CountyCode == 45 | CountyCode ==67 | CountyCode ==77 | CountyCode ==81 | CountyCode ==83 | CountyCode ==85 | CountyCode ==91 | CountyCode ==103 | CountyCode ==113)) |
@@ -92,7 +90,7 @@ cdef <- cdef %>%
 
 
 #convert to wide format
-wide<-dcast(cdef, SiteID~CharacteristicName+ResultMeasureMeasureUnitCode, value.var="ResultMeasureValue", max)
+wide<-dcast(cdef2, SiteID~CharacteristicName+ResultMeasureMeasureUnitCode, value.var="ResultMeasureValue", median)
 #write to csv
 
 write.csv("", file = "~/Desktop/.csv", row.names = FALSE)
