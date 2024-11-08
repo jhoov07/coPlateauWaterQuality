70775209bafa3af3e0dba25812059be37f34808c
aaronnuanez <aaronnuanez@arizona.edu>
2024-11-08 13:10:35 -0700

    update

--- WQP_and_NNWells.R
+++ WQP_and_NNWells.R
@@ -8,7 +8,7 @@ setwd("/Volumes/HooverShare/Shared_Group_Data/20_projects/06_coPlateau_Rework/")
 rm(list=ls())
 
 WQP <- read.csv("./02_Data/Raw_Data/WQP_As_All.csv", na.strings = "NULL")
-NNWells <- read.csv("./02_Data/Raw_Data/Clean_nnwells3_ExportTable.csv", na.strings = "NULL")
+NNWells <- read.csv("./02_Data/Raw_Data/NNWells_As_All.csv", na.strings = "NULL")
 
 As_COPLat_Data<-smartbind(WQP, NNWells)
 
