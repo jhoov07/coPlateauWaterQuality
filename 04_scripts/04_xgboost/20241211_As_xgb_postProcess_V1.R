6a5bb1eb9848d33b2046218ee603173e5f8f034e
jhoov07 <156460040+jhoov07@users.noreply.github.com>
2024-12-12 09:42:55 -0700

    new code for processing XGB output

---
+++ 04_scripts/04_xgboost/20241211_As_xgb_postProcess_V1.R
@@ -0,0 +1,147 @@
+library(caTools) 
+#library(randomForest)
+library(gbm)
+library(xgboost) # for xgboost
+library(caret)
+#library(tidyverse)
+
+#install.packages("cutpointr") #install only once then comment out
+library(cutpointr)
+
+setwd("/Users/hoover/Documents/GitHub/coPlateauWaterQuality/03_data/")
+#setwd("/Users/aaronnuanez/Documents/GitHub/coPlateauWaterQuality/03_data")
+
+rm(list=ls())
+
+#Load data
+Asdata <- read.csv("All_As_Data.csv")
+
+#Load XGB model
+#Arsenic_xgb<-readRDS("./XGB_rds/2024-12-08_ClassLTE1_cv10_xgb.rds")
+#Arsenic_xgb<-readRDS("./XGB_rds/2024-12-04_ClassLTE2_cv10_xgb.rds")
+#Arsenic_xgb<-readRDS("./XGB_rds/2024-12-05_ClassLTE3_cv10_xgb.rds")
+#Arsenic_xgb<-readRDS("./XGB_rds/2024-12-05_ClassLTE5_cv10_xgb.rds")
+Arsenic_xgb<-readRDS("./XGB_rds/2024-12-08_ClassLTE10_cv10_xgb.rds")
+Arsenic_xgb
+
+# Filter data into train and test sets based on logical variable 'trainCat2'
+#train <- Asdata[Asdata$trainClassLTE2_splt == TRUE, ] #Need up update this field and dataframe to match what is produce in lines 21-24
+#test <- Asdata[Asdata$trainClassLTE1_splt == FALSE, ] #Need up update this field and dataframe to match what is produce in lines 21-24
+#test <- Asdata[Asdata$trainClassLTE2_splt == FALSE, ] #Need up update this field and dataframe to match what is produce in lines 21-24
+#test <- Asdata[Asdata$trainClassLTE3_splt == FALSE, ] #Need up update this field and dataframe to match what is produce in lines 21-24
+#test <- Asdata[Asdata$trainClassLTE5_splt == FALSE, ] #Need up update this field and dataframe to match what is produce in lines 21-24
+test <- Asdata[Asdata$trainClassLTE10_splt == FALSE, ] #Need up update this field and dataframe to match what is produce in lines 21-24
+
+#Make SiteID the row name so we can drop that field
+#rownames(train)<-train$SiteID
+rownames(test)<-test$SiteID
+
+#Drop unused fields
+#AsTrain<-train[,-c(1, 4, 109:112, 157, 159:168)] #Drop the As concentration, and the categorical variables we already transformed
+#AsTest<-test[,-c(1, 4, 109:112, 157:160, 162:168)] #for use with LTE1
+#AsTest<-test[,-c(1, 4, 109:112, 157,159:168)] #for use with LTE2
+#AsTest<-test[,-c(1, 4, 109:112, 157:158, 160:168)] #for use with LTE3
+#AsTest<-test[,-c(1, 4, 109:112, 157:159, 161:168)] #for use with LTE5
+AsTest<-test[,-c(1, 4, 109:112, 157:160, 162:168)] #for use with LTE10
+
+#Ensure ClassLTE2 is a Factor (Categorical Variable)
+#AsTest$ClassLTE2 <- as.factor(AsTest$ClassLTE2)
+#AsTest$ClassLTE3 <- as.factor(AsTest$ClassLTE3)
+#AsTest$ClassLTE5 <- as.factor(AsTest$ClassLTE5)
+AsTest$ClassLTE10  <- as.factor(AsTest$ClassLTE10)
+
+
+#Load data
+#classifier_RF <- readRDS("2024-11-25_classLTE3_cv10_rf.rds")
+#classifier_RF
+
+# Predicting the Test set results 
+#Predictions
+y_pred = predict(Arsenic_xgb, newdata = test[,-1], type="prob") 
+#y_pred = predict(Arsenic_xgb, newdata = test[,-1], missing ="NULL"); length(y_pred)
+
+#Join probability with outcome from Test set
+#y_predJoin<-cbind(test[,157], y_pred) #change field to match outcome modeled, this applies to LT1
+#y_predJoin<-cbind(test[,158], y_pred) #change field to match outcome modeled, this applies to LT2
+#y_predJoin<-cbind(test[,159], y_pred) #change field to match outcome modeled, this applies to LT3
+#y_predJoin<-cbind(test[,160], y_pred) #change field to match outcome modeled, this applies to LT5
+y_predJoin<-cbind(test[,161], y_pred) #change field to match outcome modeled, this applies to LT10
+
+#rename fields for ease of use
+colnames(y_predJoin)[1]<-"Obsclass"
+colnames(y_predJoin)[2]<-"PrednoExceed"
+colnames(y_predJoin)[3]<-"Predexceed"
+
+cp <- cutpointr(y_predJoin, Predexceed, Obsclass, 
+                method = maximize_metric, metric = youden)
+summary(cp)
+plot(cp)
+
+#Extract ROC Curve data for plotting
+a<-as.data.frame(cp$roc_curve)
+a$sens<-a$tp/(a$tp+a$fn)
+a$spec<-a$tn/(a$tn+a$fp)
+a$j<-(a$tp/(a$tp+a$fn))+(a$tn/(a$tn+a$fp))-1
+
+##Make a plot like USGS PFAS paper S8
+ggplot()
+ggplot(a, aes(x=x.sorted, y=j)) +
+  geom_line()
+
+ggplot(a, aes(x=x.sorted)) + 
+  geom_line(aes(y = j), color = "darkred") + 
+  geom_line(aes(y = sens), color="steelblue") + 
+  geom_line(aes(y = spec), color="steelblue")
+
+#Assign predicted class using ROC threshhold 
+y_pred$class<-0
+#y_pred$class[y_pred$'1' > 0.1258]<-1 #for LTE1
+#y_pred$class[y_pred$'1' > 0.6034]<-1 #for LTE2
+y_pred$class[y_pred$'1' > 0.6023]<-1 #for LTE3
+#y_pred$class[y_pred$'1' > 0.1405]<-1 #for LTE5
+#y_pred$class[y_pred$'1' > 0.1259]<-1 #for LTE10
+
+# Confusion Matrix 
+#confusion_mtx <- confusionMatrix(factor(y_pred$class), AsTest$ClassLTE1, positive = '1')
+#confusion_mtx <- confusionMatrix(factor(y_pred$class), AsTest$ClassLTE2, positive = '1')
+confusion_mtx <- confusionMatrix(factor(y_pred$class), AsTest$ClassLTE3, positive = '1')
+#confusion_mtx <- confusionMatrix(factor(y_pred$class), AsTest$ClassLTE5, positive = '1')
+#confusion_mtx <- confusionMatrix(factor(y_pred$class), AsTest$ClassLTE10, positive = '1')
+confusion_mtx
+
+# Plotting model 
+plot(Arsenic_xgb)
+
+# Calculate Accuracy
+accuracy <- confusion_mtx$overall['Accuracy']
+accuracy
+
+# Calculate kappa value
+kappa_value <- confusion_mtx$overall['Kappa']
+kappa_value
+
+# Extract Sensitivity and Specificity for each class
+sensitivity <- confusion_mtx$byClass[[1]]
+sensitivity
+specificity <- confusion_mtx$byClass[[2]]
+specificity
+
+# training data accuracy and kappa
+# AvgAcc = accuracy  Avgkap = kappa
+Arsenic_xgb$resample %>%
+  arrange(Resample) %>%
+  mutate(AvgAcc = mean(Accuracy)) %>%
+  mutate(Avgkap = mean(Kappa))
+
+
+# Test data values
+accuracy
+kappa_value
+sensitivity
+specificity
+
+importance <- varImp(Arsenic_xgb, scale = TRUE)
+
+# Plot variable importance
+plot(importance, top = 10, col = "blue",  main = "Variable Importance, XGB, As > 3ug/L")
+
