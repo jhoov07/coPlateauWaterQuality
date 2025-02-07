30fe3d9c73dd4560c2374a2ff85b4894f1172a44
aaronnuanez <aaronnuanez@arizona.edu>
2025-02-06 17:22:53 -0700

    updates

--- 04_scripts/02_randomForest/10ugLAs/20250205_As_randomForest_step2.R
+++ 04_scripts/02_randomForest/10ugLAs/20250205_As_randomForest_step2.R
@@ -4,8 +4,8 @@ library(caret)
 library(tidyverse)
 
 
-setwd("/Users/hoover/Downloads")
-#setwd("/Users/aaronnuanez/Documents/GitHub/coPlateauWaterQuality/03_data")
+#setwd("/Users/hoover/Downloads")
+setwd("/Users/aaronnuanez/Desktop/")
 
 
 rm(list=ls())
@@ -14,24 +14,24 @@ rm(list=ls())
 Asdata <- read.csv("All_As_Data.csv")
 
 # Filter data into train and test sets based on logical variable 'trainCat2'
-train <- Asdata[Asdata$trainClassLTE2_splt == TRUE, ] #Need up update this field and dataframe to match what is produce in lines 21-24
-test <- Asdata[Asdata$trainClassLTE2_splt == FALSE, ] #Need up update this field and dataframe to match what is produce in lines 21-24
+train <- Asdata[Asdata$trainClassLTE10_splt == TRUE, ] #Need up update this field and dataframe to match what is produce in lines 21-24
+test <- Asdata[Asdata$trainClassLTE10_splt == FALSE, ] #Need up update this field and dataframe to match what is produce in lines 21-24
 
 #Make SiteID the row name so we can drop that field
 rownames(train)<-train$SiteID
 rownames(test)<-test$SiteID
 
 #Drop unused fields
-AsTrain<-train[,-c(1, 4, 109:112, 157, 159:168)] #Drop the As concentration, and the categorical variables we already transformed
-AsTest<-test[,-c(1, 4, 109:112, 157, 159:168)]
+AsTrain<-train[,-c(1, 4, 109:112, 157:160, 162:168)] #Drop the As concentration, and the categorical variables we already transformed
+AsTest<-test[,-c(1, 4, 109:112, 157:160, 162:168)]
 
-#Ensure ClassLTE2 is a Factor (Categorical Variable)
-AsTrain$ClassLTE2 <- as.factor(AsTrain$ClassLTE2)
-AsTest$ClassLTE2  <- as.factor(AsTest$ClassLTE2)
+#Ensure ClassLTE10 is a Factor (Categorical Variable)
+AsTrain$ClassLTE10 <- as.factor(AsTrain$ClassLTE10)
+AsTest$ClassLTE10  <- as.factor(AsTest$ClassLTE10)
 
 
 #Load data
-classifier_RF <- readRDS("2025-01-31_rf.rds")
+classifier_RF <- readRDS("2025-02-06_rf_10ugL.rds")
 classifier_RF
 
 #best mtry is 54
@@ -40,7 +40,7 @@ classifier_RF
 y_pred <- predict(classifier_RF, newdata = AsTest)
 
 # Confusion Matrix 
-confusion_mtx <- confusionMatrix(y_pred, AsTest$ClassLTE2)
+confusion_mtx <- confusionMatrix(y_pred, AsTest$ClassLTE10)
 confusion_mtx
 
 #This output gives us the best model using all variables. Next step is to apply variable selection and use fewer predictors
