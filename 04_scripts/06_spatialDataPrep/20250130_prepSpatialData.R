c0007fa6ab67108e3c1f684293508211bafd4b2d
aaronnuanez <aaronnuanez@arizona.edu>
2025-02-14 11:41:53 -0700

    updates

--- 04_scripts/06_spatialDataPrep/20250130_prepSpatialData.R
+++ 04_scripts/06_spatialDataPrep/20250130_prepSpatialData.R
@@ -9,16 +9,18 @@
 #10 ug/L model, Arsenic
 #Key variables - "A_Aragon_500m.tif"   "A_Calcite_500m.tif"  "A_Cs_500m.tif"       "A_Tot_14A.tif"       "C_Amorph.tif"        "C_Analcime.tif"     
 #[7] "C_Cr_500m.tif"       "C_Hematite_500m.tif" "C_Mo.tif"            "DepthToGW_V2.tif"    "fe_v2.tif"           "pH_v2.tif"          
-#[13] "prism30yr_500m.tif"  "Top5_Ca.tif"         "Top5_S_500m.tif" 
+#[13] "prism30yr_500m.tif"  "Top5_Ca.tif"         "Top5_S_500m.tif"    "Top5_Be.tif"    "Top5_Ni.tif"
 
 #5 ug/L model
 #[1] "A_C_Tot.tif"        "A_Calcite.tif"      "A_Hg.tif"           "A_Kaolinit.tif"     "A_Quartz.tif"       "A_Tl.tif"          
 #[7] "A_Tot_Flds.tif"     "C_Cr.tif"           "C_Hematite.tif"     "C_Kaolinit.tif"     "C_Sb.tif"           "C_Se.tif"          
 #[13] "C_Tot_14A.tif"      "DepthToGW_500m.tif" "Fe_500m.tif"        "pH_500m.tif"        "prism30yr_500m.tif" "Top5_As.tif"       
-#[19] "Top5_Ba.tif"        "Top5_Ca.tif"  
+#[19] "Top5_Ba.tif"        "Top5_Ca.tif"       "C_Tot_K_fs.tif"
 
 rm(list=ls()) #clean up the workspace
-wd <- ("/Users/hoover/desktop/tifsForModeling") #write using local folder since the files are too large for GitHub
+#wd <- ("/Users/hoover/desktop/tifsForModeling") #write using local folder since the files are too large for GitHub
+wd <- ("/Users/aaronnuanez/desktop/tifsForModeling") #write using local folder since the files are too large for GitHub
+
 
 
 ## ----list-files-tif--------------------------------------------------
@@ -48,6 +50,8 @@ C_Mo<-rast(paste(wd,"/C_Mo.tif", sep=""))
 C_Sb<-rast(paste(wd,"/C_Sb.tif", sep=""))
 C_Se<-rast(paste(wd,"/C_Se.tif", sep=""))
 C_Tot_14A<-rast(paste(wd,"/C_Tot_14A.tif", sep=""))
+C_Tot_K_s<-rast(paste(wd,"/C_Tot_K_s.tif", sep=""))
+
 
 DepthToGW<-rast(paste(wd,"/DepthToGW_V2.tif", sep=""))
 Fe<-rast(paste(wd,"/fe_v2.tif" , sep=""))
@@ -58,6 +62,9 @@ Top5_As<-rast(paste(wd,"/Top5_As.tif", sep=""))
 Top5_Ba<-rast(paste(wd,"/Top5_Ba.tif", sep=""))
 Top5_Ca<-rast(paste(wd,"/Top5_Ca.tif", sep=""))
 Top5_S<-rast(paste(wd,"/Top5_S_500m.tif", sep=""))
+Top5_Ni<-rast(paste(wd,"/Top5_Ni.tif", sep=""))
+Top5_Be<-rast(paste(wd,"/Top5_Be.tif", sep=""))
+
 
 #<-rast(paste(wd,".tif", sep=""))
 
@@ -82,6 +89,8 @@ C_Mo<-resample(C_Mo, Fe, method = "bilinear")
 C_Sb<-resample(C_Sb, Fe, method = "bilinear")
 C_Se<-resample(C_Se, Fe, method = "bilinear")
 C_Tot_14A<-resample(C_Tot_14A, Fe, method = "bilinear")
+C_Tot_K_fs<-resample(C_Tot_K_fs, Fe, method = "bilinear")
+
 
 DepthToGW<-resample(DepthToGW, Fe, method = "bilinear")
 pH<-resample(pH, Fe, method = "bilinear")
@@ -91,6 +100,9 @@ Top5_As<-resample(Top5_As, Fe, method = "bilinear")
 Top5_Ba<-resample(Top5_Ba, Fe, method = "bilinear")
 Top5_Ca<-resample(Top5_Ca, Fe, method = "bilinear")
 Top5_S<-resample(Top5_S, Fe, method = "bilinear")
+Top5_Be<-resample(Top5_Be, Fe, method = "bilinear")
+Top5_Ni<-resample(Top5_Ni, Fe, method = "bilinear")
+
 
 
 #crop
@@ -114,6 +126,8 @@ C_Mo<-crop(C_Mo, Fe, mask = TRUE)
 C_Sb<-crop(C_Sb, Fe, mask = TRUE)
 C_Se<-crop(C_Se, Fe, mask = TRUE)
 C_Tot_14A<-crop(C_Tot_14A, Fe, mask = TRUE)
+C_Tot_K_fs<-crop(C_Tot_K_fs, Fe, mask = TRUE)
+
 
 DepthToGW<-crop(DepthToGW, Fe, mask = TRUE)
 pH<-crop(pH, Fe, mask = TRUE)
@@ -123,6 +137,9 @@ Top5_As<-crop(Top5_As, Fe, mask = TRUE)
 Top5_Ba<-crop(Top5_Ba, Fe, mask = TRUE)
 Top5_Ca<-crop(Top5_Ca, Fe, mask = TRUE)
 Top5_S<-crop(Top5_S, Fe, mask = TRUE)
+Top5_Be<-crop(Top5_Be, Fe, mask = TRUE)
+Top5_Ni<-crop(Top5_Ni, Fe, mask = TRUE)
+
 
 
 
@@ -161,6 +178,11 @@ writeRaster(Top5_Ba, paste(d, "Top5_Ba.tif", sep=""))
 
 writeRaster(Top5_Ca, paste(d, "Top5_Ca.tif", sep=""))
 writeRaster(Top5_S, paste(d, "Top5_S.tif", sep=""))
+writeRaster(C_Tot_K_fs, paste(d, "C_Tot_K_fs.tif", sep=""))
+writeRaster(Top5_Be, paste(d, "Top5_Be.tif", sep=""))
+writeRaster(Top5_Ni, paste(d, "Top5_Ni.tif", sep=""))
+
+
 
 #Reference raster
 writeRaster(Fe, paste(d, "Fe.tif", sep=""))
